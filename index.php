185fce6c5c0fc6a0463c246aff7f441c3906df22
ddifof <ddifof@gmail.com>
2017-02-23 03:32:34 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,2 +1,2 @@
 <?php
-echo '<p>CloudForms Demo!<a href="http://cloudformsblog.redhat.com/">http://cloudformsblog.redhat.com/</a></p>';
+echo '<p> Welcome to the CloudForms + Ansible Demo! Find more information on <a href="http://cloudformsblog.redhat.com/">http://cloudformsblog.redhat.com/</a></p>';
